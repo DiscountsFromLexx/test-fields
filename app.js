bac99829cc05def9c743640ea136a30a1548c66a
DISCOUNTS-FROM-ALI <dyntchev@gmail.com>
2025-01-21 09:37:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const field2 = document.getElementById('field2').value;
         const field3 = document.getElementById('field3').value;
         const field4 = document.getElementById('field4').value;
+        const field4 = document.getElementById('field5').value;
 
         console.log("Field 1:", field1);
         console.log("Field 2:", field2);
         console.log("Field 3:", field3);
         console.log("Field 3:", field4);
+        console.log("Field 3:", field5);
 
         // Here you can add the logic to process the input data or send it to a server
     });
